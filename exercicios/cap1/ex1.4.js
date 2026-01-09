0d497f9a9152138e767f37ea4f6f8e35df5be2ef
Tainá Vitória <taina2391@gmail.com>
2026-01-09 18:57:15 -0300

    update

---
+++ exercicios/cap1/ex1.4.js
@@ -0,0 +1,11 @@
+// TIPOS DE VARIÁVEIS //
+ 
+const fruta = "Maçã" 
+const preco = 5.50 
+const verificar = true 
+let tipo 
+
+console.log(typeof fruta) /*answer: string*/
+console.log(typeof preco) /*answer: number*/
+console.log(typeof verificar) /*answer: boolean*/
+console.log(typeof tipo) /*answer: undefined*/
