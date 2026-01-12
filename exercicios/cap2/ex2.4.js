5af12d7d204f326306bbb5cc8c6807e57dafa32c
Tainá Vitória <taina2391@gmail.com>
2026-01-12 18:38:22 -0300

    update

---
+++ exercicios/cap2/ex2.4.js
@@ -0,0 +1,17 @@
+const formulario = document.querySelector("form")
+const rsp1 = document.querySelector("#resp1")
+const rsp2 = document.querySelector("#resp2")
+
+// ação do botão "ver promoções"
+
+formulario.addEventListener("submit", (e) => {
+    const remedio = formulario.inRemedio.value
+    const preco = Number(formulario.inPreco.value)
+
+    const desconto = ((preco * 2) * 100) / 100
+
+    rsp1.innerHTML = `Promoção de ${remedio}`
+    rsp2.innerHTML = `Na compra de dois, pague somente R$ ${desconto}`
+
+    e.preventDefault()
+})
